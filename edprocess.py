3ac10417831398a61fe53751f69939115557fd61
Amauri de Oliveira Albuquerque <amauriala@opencloud.com>
2016-04-12 16:04:05 +1200

    start the code to deal with ed messages

--- edprocess.py
+++ edprocess.py
@@ -8,6 +8,15 @@ import canmodule
 
 #class that deals with the ED messages, basically this class holds the client socket and the major code
 
+SOFT_VERSION = "VN2.0"
+ROASTER_INFO = "RL0]|[" #no locos
+START_INFO = "VN2.0\n\rRL0]|[\n\rPPA1\n\rPTT]|[\n\rPRT]|[\n\rRCC0\n\rPW080\n\r"
+DELIM_BRACET = "]|["
+DELIM_BTLT = "<;>"
+DELIM_KEY = "}|{"
+EMPTY_LABES = "<;>]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[]\[" #MTS3<.....
+
+
 class TcpClientHandler(threading.Thread):
 
     # client is the tcp client
@@ -18,7 +27,7 @@ class TcpClientHandler(threading.Thread):
         self.can = canwriter
         self.address = address
         self.canmsg = []
-        self.sessions = [] #array of sessions
+        self.sessions = {} #dict of sessions; key is the loco number
         self.running = True
 
     #receive all the can message
@@ -35,6 +44,11 @@ class TcpClientHandler(threading.Thread):
 
     def run(self):
         logging.info("serving the tcp client %s" % self.address[0])
+
+        #send the version software info and the throttle data
+        logging.debug("Sending start info :%s" %START_INFO)
+        self.sendClientMessage(START_INFO.encode('utf-8'))
+
         size = 1024
         while self.running:
             try:
@@ -55,10 +69,56 @@ class TcpClientHandler(threading.Thread):
         logging.debug("Tcp server closing client socket for %(self.address)s " %locals())
         self.client.close()
 
-    def handleEdMessages(self,message):
+    def handleEdMessages(self, message):
         logging.debug("handling the client message :%" %message)
+        #get the name
+        if message[0] == 'A':
+            self.edname = message[1:]
+            #send the can data
+            logging.debug("put CAN session set timer in queue")
+            #TODO
+            self.can.put("*10") #keep alive each 10 seconds
+            return
+
+        #get hardware info
+        if message[0:1] == "HU"
+            self.hwinfo = message[2:]
+            return
+
+        #get the session request
+        if message[0:1] == "MT": #multi throttle
+            #MT+S3<;>
+            if message[2] == '+': #create session
+                if message[3] in ["S","s","L","l"]:
+                    adtype = message[3]
+                    #get loco
+                    i = message.find("<")
+                    loco = message[4:(i-1)]
+                    edsession = EdSession(0,loco,adtype)
+                    self.sessions[loco] = edsession
+                    #send the can data
+                    logging.debug("put CAN session request in the queue")
+                    #TODO
+                    self.can.put("request session")
+                    return
+
+
+
+
+
+    def sendClientMessage(self, message):
+        self.client.sendall(message)
 
 class EdSession:
-    def __init__(self,sessionid):
+    def __init__(self,sessionid,loco,adtype):
         self.sessionid = sessionid
         self.keepalivetime = 0
+        self.loco = loco
+        self.adtype = adtype #S =short address or L = long adress
+
+        def getSessionId:
+            return self.sessionid
+        def getLoco:
+            return self.loco
+        def getAdType:
+            return self.adtype
