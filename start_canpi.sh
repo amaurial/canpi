a69b80518efbc0cbac06779ae21238dbbf6a20c5
Amauri <amaurial@uol.com.br>
2016-06-09 00:11:03 +1200

    Change the project structure by putting the python code in a new dir. Add new configuration in the canpi.cfg The script start_canpi.sh now reads the canpi.cfg to configure ap mode or wifi mode, the bonjour name and port and the wifi channel,ssid and password.

--- start_canpi.sh
+++ start_canpi.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ### BEGIN INIT INFO
 # Provides: canpi
 # Required-Start:    $remote_fs $syslog $network
@@ -9,8 +9,11 @@
 # Description:       Enable service provided by Merg canpi daemon.
 ### END INIT INFO
 
-dir="/home/pi/canpi/c/canpi"
-cmd="/home/pi/canpi/c/canpi/canpi"
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+. /lib/lsb/init-functions
+
+dir="/home/pi/canpi"
+cmd="/home/pi/canpi/canpi"
 config="${dir}/canpi.cfg"
 user=""
 
@@ -20,8 +23,29 @@ stdout_log="/var/log/$name.log"
 stderr_log="/var/log/$name.err"
 
 source $config
-bonjour_file="/etc/avahi/services"
-bonjour_template="$dir/services"
+
+#Bonjour files
+bonjour_file="/etc/avahi/services/multiple.service"
+bonjour_template="$dir/multiple.service"
+
+#wpa supplicant files
+wpa_file="/etc/wpa_supplicant/wpa_supplicant.conf"
+wpa_template="$dir/wpa_supplicant.conf"
+
+#hostapd files
+hostapd_file="/etc/hostapd/hostapd.conf"
+hostapd_template="$dir/hostapd.conf"
+default_hostapd_file="/etc/default/hostapd"
+safe_hostapd_file="$dir/hostapd"
+
+#dhcp files
+default_dhcp_file="/etc/default/isc-dhcp-server"
+safe_dhcp_file="$dir/isc-dhcp-server"
+
+#interface files
+iface_file="/etc/network/interfaces"
+iface_file_wifi="$dir/interfaces.wifi"
+iface_file_ap="$dir/interfaces.ap"
 
 
 get_pid() {
@@ -32,24 +56,40 @@ is_running() {
     [ -f "$pid_file" ] && ps `get_pid` > /dev/null 2>&1
 }
 
-setup_bonjour(){
+is_avahi_running(){
+    r=`/etc/init.d/avahi-daemon status`
+    echo $r | grep "active (running)"
+}
+
+is_dhcp_running(){
+    r=`/etc/init.d/isc_dhcp-server status`
+    echo $r | grep "active (running)"
+}
+
+is_hostapd_running(){
+    r=`/etc/init.d/hostapd status`
+    echo $r | grep "active (running)"
+}
+
+setup_bonjour() {
     echo "Configuring the bonjour service"
     #back the old file
     sudo mv $bonjour_file "${bonjour_file}.bak"
-    sudo mv $bonjour_template $bonjour_file
+    sudo cp $bonjour_template $bonjour_file
 
     #change the service name
-    sudo sed -i 's/SERVICENAME/${service_name}/' $bonjour_file
+    sudo sed -i 's/SERVICENAME/'"$service_name"'/' $bonjour_file
 
     #change the port
-    sudo sed -i 's/PORT/${service_name}/' $tcpport
+    sudo sed -i 's/PORT/'"$tcpport"'/' $bonjour_file
 
     #restart the service
     echo "Restarting the bonjour service"
     sudo /etc/init.d/avahi-daemon restart
-    r = `/etc/init.d/avahi-daemon status`
-    echo $r|grep "avahi-daemon start/running"
-    if [ $? == 0 ]
+    sleep 1
+    #r=`/etc/init.d/avahi-daemon status`
+    #echo $r | grep "active (running)"
+    if is_avahi_running;
     then
         echo "Bonjour restarted"
     else
@@ -57,27 +97,134 @@ setup_bonjour(){
     fi
 }
 
-setup_ap_mode(){
-#set static ip
-#start dhcp service
-#start hostapd service
+setup_wpa_supplicant(){
+    echo "Configuring the WPA supplicant"
+    #back the old file
+    sudo mv $wpa_file "${wpa_file}.bak"
+    sudo cp $wpa_template $wpa_file
+
+    #change the ssid
+    sudo sed -i 's/SSID/'"$router_ssid"'/' $wpa_file
+
+    #change the password
+    sudo sed -i 's/PASSWORD/'"$router_password"'/' $wpa_file
+ 
+}
+
+setup_hostapd(){
+    echo "Configuring the hostapd"
+    #back the old file
+    sudo mv $hostapd_file "${hostapd_file}.bak"
+    sudo cp $hostapd_template $hostapd_file
+    
+    #change the ssid
+    sudo sed -i 's/SSID/'"$ap_ssid"'/' $hostapd_file
+    
+    #change the password
+    sudo sed -i 's/PASSWORD/'"$ap_password"'/' $hostapd_file
+ 
+    #change the channel
+    sudo sed -i 's/CHANNEL/'"$ap_channel"'/' $hostapd_file
+}
+
+setup_iface_wifi(){
+    #back the old file
+    sudo mv $iface_file "${iface_file}.bak"
+    sudo cp $iface_file_wifi $iface_file
+}
+
+setup_iface_ap(){
+    #back the old file
+    sudo mv $iface_file "${iface_file}.bak"
+    sudo cp $iface_file_ap $iface_file
+}
+
+remove_hostapd_default(){
+    #back the old file
+    sudo mv $default_hostapd_file "${default_hostapd_file}.bak"
+}
+
+remove_dhcp_default(){
+    #back the old file
+    sudo mv $default_dhcp_file "${default_dhcp_file}.bak"
+}
+
+restore_hostapd_default(){
+    #back the old file
+    sudo cp $safe_hostapd_file $default_hostapd_file
+}
+
+restore_dhcp_default(){
+    #back the old file
+    sudo cp $safe_dhcp_file $default_dhcp_file
+}
+
+setup_ap_mode()
+{
+    echo "Configuring to AP mode"
+    
+    
+    sleep 1
+    sudo /etc/init.d/hostapd stop
+    sleep 1
+    sudo /etc/init.d/isc-dhcp-server stop
+    
+    
+    echo "Restarting wlan0"
+    sudo ip link set wlan0 down
+    setup_iface_ap
+    setup_hostapd
+    sleep 1
+    sudo ip link set wlan0 up
+
+    sleep 2
+    sudo /etc/init.d/hostapd start
+    sleep 1
+    sudo /etc/init.d/isc-dhcp-server start
+    
+    echo "Configuring the hostapd daemon"
+    sudo /usr/sbin/update-rc.d hostapd defaults
+    echo "Configuring DHCP server" 
+    sudo /usr/sbin/update-rc.d isc-dhcp-server defaults
 }
 
 setup_wifi_mode(){
-#set wlan to dhcp
-#stop dhcp service
-#stop hostapd
+    echo "Configuring to wifi mode"
+    setup_iface_wifi
+    setup_wpa_supplicant
+     
+    echo "Stoping wlan0"
+    sudo ip link set wlan0 down
+    
+    echo "Stoping the hostapd service"
+    sudo /etc/init.d/hostapd stop    
+    sleep 1
+    
+    echo "Stoping the DHCP service"
+    sudo /etc/init.d/isc-dhcp-server stop
+    sleep 1
+
+    #echo "Starting wlan0"
+    #sudo ip link set wlan0 up
+    #sudo /etc/init.d/networking rstart
+    #sleep 1
+    #remove_hostapd_default
+    #remove_dhcp_default
+    sudo /usr/sbin/update-rc.d hostapd remove 
+    sleep 1
+    sudo /usr/sbin/update-rc.d isc-dhcp-server remove
 }
 
 start_led_watchdog(){
-
+   echo "watchdog"
 }
 
 stop_led_watchdog(){
-
+   echo "watchdog"
 }
 
 bootstrap(){
+   echo "bootstrap"
 }
 
 
@@ -90,8 +237,8 @@ case "$1" in
         cd "$dir"
 
         #restart can interface
-        sudo /sbin/ip link set can0 down
-        sudo /sbin/ip link set can0 up type can bitrate 125000 restart-ms 1000
+        #sudo /sbin/ip link set can0 down
+        #sudo /sbin/ip link set can0 up type can bitrate 125000 restart-ms 1000
 
         if [ -z "$user" ]; then
             sudo $cmd >> "$stdout_log" 2>> "$stderr_log" &
@@ -144,7 +291,17 @@ case "$1" in
     ;;
     configure)
         echo "Configuring the services"
-        setup_bonjour()
+        if [ $ap_mode == "true" ];then
+           echo "Starting configuration for AP Mode"
+           setup_ap_mode
+        else
+           echo "Starting configuration for Wifi Mode"
+           setup_wifi_mode
+        fi
+        setup_bonjour
+        sleep 5
+        echo "Rebooting"
+        sudo reboot
     ;;
     status)
     if is_running; then
